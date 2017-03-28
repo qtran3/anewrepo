5e0a92d221860caaca41e97990c5416b1e2f0e71
qtran3 <qtran3@ggc.edu>
2017-03-27 20:13:34 -0400

    a new commit

--- 2016AITP/src/main/java/sf/codingcomp/exception/TitleNotAvailableException.java
+++
@@ -1,7 +0,0 @@
-package sf.codingcomp.exception;
-
-public class TitleNotAvailableException extends RuntimeException {
-
-    private static final long serialVersionUID = -5823786826475345070L;
-
-}
